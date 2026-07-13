4a3ded4a2ff49a229be65a4403901f23162c1fc3
krisxu23 <92844112+krisxu23@users.noreply.github.com>
2026-07-13 17:19:14 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -48,9 +48,9 @@ public final class NanoLimbo {
         // ================================================================
         ServerConfig config = ServerConfig.getInstance();
         config.setUuid("5c002620-79a3-4417-bc96-86490f2c2fbd");  // 节点UUID，不同平台部署需更改
-        config.setDomain("78.154.103.24");                          // 服务器域名或IP
+        config.setDomain("");                          // 服务器域名或IP
         config.setPort("25565");                                  // Minecraft伪装端口
-        config.setRemarksPrefix("xah");                           // 节点备注前缀
+        config.setRemarksPrefix("rustix");                           // 节点备注前缀
 
         // sing-box 版本
         config.setSbVersion("1.13.14");                           // sing-box版本号
@@ -62,8 +62,8 @@ public final class NanoLimbo {
         config.setArgoVersion("2025.10.0");                       // cloudflared版本号
 
         // 各协议端口配置（留空=不启用，填端口=启用）
-        config.setRealityPort("14649");                                // VLESS+Reality端口(TCP)
-        config.setHy2Port("14649");                                    // Hysteria2端口(UDP)
+        config.setRealityPort("33959");                                // VLESS+Reality端口(TCP)
+        config.setHy2Port("33959");                                    // Hysteria2端口(UDP)
         config.setTuicPort("");                                   // TUIC端口(UDP)
         config.setSocks5Port("");                                 // SOCKS5端口(TCP)
         config.setAnytlsPort("");                                 // AnyTLS端口(TCP)
